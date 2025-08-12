879ad50c56db33aa8825267e21cf9329fabd2a48
peteksnr <117526538+peteksnr@users.noreply.github.com>
2025-08-12 22:33:14 +0300

    Update score_board.py

--- score_board.py
+++ score_board.py
@@ -32,10 +32,6 @@ class ScoreBoard(Turtle):
         self.score = 0
         self.update_scoreboard()
 
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write(arg="GAME OVER", align=ALIGNMENT, font=FONT)
-
     def track_score(self):
         self.score += 1
         self.update_scoreboard()
